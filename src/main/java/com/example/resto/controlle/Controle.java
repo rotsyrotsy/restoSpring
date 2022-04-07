aa63f1dff93de21808e1a7055570774775d8079b
Junx2001 <ratsirarsonj@gmail.com>
2022-04-07 12:48:33 +0300

    Update Controle.java

--- src/main/java/com/example/resto/controlle/Controle.java
+++ src/main/java/com/example/resto/controlle/Controle.java
@@ -5,10 +5,18 @@
  */
 package com.example.resto.controlle;
 
+import java.util.Date;
+
 /**
  *
  * @author ratsi
  */
 public class Controle {
-    public 
+    public void controleDate(Date date1,Date date2) throws IllegalStateException
+    {
+        if(date1.compareTo(date2)>0)
+        {
+            throw new IllegalStateException("Date Début supérieur à Date Fin");
+        }
+    }
 }
