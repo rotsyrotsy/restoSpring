f4fc1a067b4849a49313e5c57aec8531d3809eba
unknown <rafa.rotsy@gmail.com>
2022-04-07 15:23:27 +0300

    Update database-0007.sql

--- script/database-0007.sql
+++ script/database-0007.sql
@@ -10,8 +10,12 @@ create table payement
 
 create sequence seqPayement;
 
-insert into payement values(nextval('seqPayement'),'1',15000,'2022-04-01','cheque');
-insert into payement values(nextval('seqPayement'),'2',175000,'2022-04-02','espece');
-insert into payement values(nextval('seqPayement'),'4',120400,'2022-04-09','cheque');
+insert into payement values(nextval('seqPayement'),'1',15000,'2022-04-06','cheque');
+insert into payement values(nextval('seqPayement'),'2',120400,'2022-04-06','cheque');
+insert into payement values(nextval('seqPayement'),'3',200000,'2022-04-07','cheque');
+insert into payement values(nextval('seqPayement'),'4',150000,'2022-04-07','cheque');
 
 
+insert into payement values(nextval('seqPayement'),'5',50000,'2022-04-06','espece');
+insert into payement values(nextval('seqPayement'),'6',30000,'2022-04-07','espece');
+insert into payement values(nextval('seqPayement'),'7',85000,'2022-04-07','espece');
