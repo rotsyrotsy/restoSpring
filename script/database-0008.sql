1c03af4be7e5d0e9b2cbbfe7053aa9ed4bafa480
Princia08 <80781644+Princia08@users.noreply.github.com>
2022-04-11 14:56:58 +0300

    Update database-0008.sql

--- script/database-0008.sql
+++ script/database-0008.sql
@@ -1,3 +1,7 @@
-
 create view payementEffectue as
-	select sum(valeur) as prix from payement p group by idOrder
+	select idOrder, sum(valeur) as prix from payement p group by idOrder
+	
+create view additionNonPaye as
+	select daty as date,pe.prix as total,po.prix - pe.prix as restant from payementEffectue pe 
+		join prixOrder po on pe.idOrder = po.idOrder
+		join orderr o on o.id = pe.idOrder
